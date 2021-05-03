f29768cc9be9694c6469b443050dc2c5c5ebcf6e
antonmaenpaa <antonmaenpaa@gmail.com>
2021-05-03 20:47:56 +0200

    join page title font weight

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -6,8 +6,8 @@ function Join() {
 
     return(
         <div className="join-div">
-            <h2 style={{ fontSize: "3rem", zIndex: 1 }}>WELCOME TO WHADDUP</h2>
-            <h2 style={{ fontSize: "3rem", marginTop: "-5.5rem", color: "#4d3f6f", }}>WELCOME TO WHADDUP</h2>
+            <h2 style={{ fontSize: "3rem", zIndex: 1, fontWeight: 700 }}>WELCOME TO WHADDUP</h2>
+            <h2 style={{ fontSize: "3rem", marginTop: "-5.5rem", color: "#4d3f6f", fontWeight: 700 }}>WELCOME TO WHADDUP</h2>
             
             <form className="form">
                 <div className="input-div">
