d239b9a09341ab052b816251fbc3a3467eb5bb08
FIRST_GUSTAV LAST_ANDERSSON <Gustavkarlandersson@hotmail.com>
2021-05-04 11:24:53 +0200

    maybe all server sockets we need for now
    Co-authored-by: Anton Mäenpää <antonmaenpaa@gmail.com>

--- server/server.js
+++ server/server.js
@@ -1,20 +1,63 @@
 const express = require('express');
-const http = require("http");
-const { Server } = require("socket.io");
+const http = require('http')
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server)
+const socket = require('socket.io');
+const io = socket(server)
 
-// app.use(express.static("client"))
+let users = [];
 
-const port = 3000;
+const rooms = {
+    general: [],
+}
 
 io.on("connection", (socket) => {
-    console.log("Client was connected", socket.id)
-})
+    socket.on("join server", (username) => {
+        const user = {
+            username,
+            id: socket.id,
+        };
+        users.push(user);
+        io.emit("new user", users)
+    })
+
+    socket.on("join room", (roomName, callback) => {
+        socket.join(roomName);
+        callback(rooms[roomName]);
+    });
+
+    socket.on("send message" , ({ content, to, sender, chatName, isChannel }) => {
+        if (isChannel) {
+            const payload = {
+                content,
+                chatName,
+                sender,
+            };
+            socket.to(to).emit("new message", payload);
+        } else {
+            const payload = {
+                content,
+                chatName: sender,
+                sender,
+            };
+            socket.to(to).emit("new message", payload);
+        }
+        if(rooms[chatName]) {
+            rooms[chatName].push({
+                sender,
+                content
+            });
+        }
+    });
+
+    socket.on("disconnect", () => {
+        users = users.filter(u => u.id ==! socket.id);
+        io.emit("new user", users);
+    })
+});
 
 
 
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+server.listen(5000, () => {
+    console.log(`Server is running on port 5000`)
 })
